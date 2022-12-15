4b7b63a4ef19edfa1618821e2d377ed01952ce8d
Yin-De <ayindemuhammad2004@gmail.com>
2022-12-15 15:38:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ start()
     console.log("port listening on 5000");
   })  , {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://dancing-manatee-f94c7b.netlify.app",
       credentials: true,
     },
 });
