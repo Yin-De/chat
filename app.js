0e0cd176aa293c34f0ff84b4e379da431f885e46
Yin-De <111071078+Yin-De@users.noreply.github.com>
2022-12-17 16:41:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ start()
     console.log("port listening on 5000");
   })  , {
     cors: {
-      origin: "https://chat-app-c4022.web.app",
+      origin: "https://chat-app-c4022.firebaseapp.com",
       credentials: true,
     },
 });
